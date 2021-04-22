28dad845e8f4d01bcabbcab18e70d89773329190
PIPPIP <kulthida.hiwmama@mail.ac.th>
2021-04-23 03:15:06 +0700

    [UPDATE] : sign-in& sign-up page (button style)

--- lib/si_su.dart
+++ lib/si_su.dart
@@ -26,7 +26,7 @@ class SplashScreenState extends State<MyHomePage> {
   void initState() {
     super.initState();
     Timer(
-        Duration(seconds: 2),
+        Duration(seconds: 8),
         () => Navigator.pushReplacement(
             context, MaterialPageRoute(builder: (context) => Sin_Sout_Page())));
   }
@@ -34,11 +34,11 @@ class SplashScreenState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(150),
+      padding: EdgeInsets.all(130),
       child: Image.asset(
         'images/cheese3.gif',
-        width: 50,
-        height: 50,
+        width: 100,
+        height: 100,
       ),
       color: Color(0xFFFEF391),
     );
@@ -53,18 +53,21 @@ class Sin_Sout_Page extends StatelessWidget {
       child: Column(
         children: [
           Container(
-              margin: const EdgeInsets.only(top: 300, bottom: 35),
+              margin: const EdgeInsets.only(top: 300, bottom: 25),
               child: Image.asset(
                 'images/cheese.png',
                 width: 100,
                 height: 100,
               )),
           ButtonBar(
+            buttonPadding: EdgeInsets.only(left: 25, right: 25),
             alignment: MainAxisAlignment.center,
             children: <Widget>[
               RaisedButton(
                 onPressed: () => {},
                 color: Color(0xffffc66c),
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(20.0)),
                 child: Text(
                   'sign up',
                   style: TextStyle(color: Colors.black),
@@ -73,6 +76,8 @@ class Sin_Sout_Page extends StatelessWidget {
               RaisedButton(
                 onPressed: () => {},
                 color: Color(0xffffc66c),
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(20.0)),
                 child: Text(
                   'sign in',
                   style: TextStyle(color: Colors.black),
