10695b3415e29554843f8e840f6976f7c44069c9
M <punnapatkh191@gmail.com>
2021-07-28 14:21:02 +0700

    Veryๆๆ Final โครตๆๆๆ

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:csc234_project_cheese/main.dart';
+import '../lib/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
